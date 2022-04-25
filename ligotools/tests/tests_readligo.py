ea86bf2920cdeaae89f4d36ef3b0092319f6e77b
Brandon Hong <hongbrandon333@berkeley.edu>
2022-04-25 22:00:03 +0000

    added tests

--- ligotools/tests/tests_readligo.py
+++ ligotools/tests/tests_readligo.py
@@ -9,29 +9,33 @@ strain_L1, time_L1, chan_dict_L1 = rl.loaddata('data/L-L1_LOSC_4_V2-1126259446-3
 def test_loadH1_strain_data():
     try:
         assert np.isclose(strain_H1[0], 2.177040281449375e-19)
+        assert strain_H1.shape == (131072, )
     except AssertionError as detail:
-        print('The strain value loaded from the data is incorrect')
+        print('The strain data loaded from the H1 dataset is incorrect')
         quit()
         
 def test_loadH1_time_data()
     try:
         assert np.isclose(time_H1[0], 1126259446.0)
+        assert time_H1.shape == (131072, )
     except AssertionError as detail:
-        print('The time value loaded from the data is incorrect')
+        print('The time data loaded from the H1 dataset is incorrect')
         quit()
 
 def test_loadL1_strain_data():
     try:
         assert np.isclose(strain_L1[0], -1.0428999418774637e-18)
+        assert strain_L1.shape == (131072, )
     except:
-        print('The strain value loaded from the data is incorrect')
+        print('The strain data loaded from the L1 data is incorrect')
         quit()
 
 def test_loadL1_time_data():
     try:
         assert np.isclose(time_L1[0], 1126259446.0)
+        assert time_L1.shape == (131072, )
     except AssertionError as detail:
-        print('The time value loaded from the data is incorrect')
+        print('The time data loaded from the L1 data is incorrect')
         quit()
 
     
--- ligotools/tests/tests_utils.py
+++ ligotools/tests/tests_utils.py
@@ -1,23 +1,43 @@
 from ligotools.utils import *
 import numpy as np
 from ligotools import readligo as rl
+from scipy.interpolate import interp1d
 
 strain_H1, time_H1, chan_dict_H1 = rl.loaddata('data/H-H1_LOSC_4_V2-1126259446-32.hdf5', 'H1')
 strain_L1, time_L1, chan_dict_L1 = rl.loaddata('data/L-L1_LOSC_4_V2-1126259446-32.hdf5', 'L1')
 
-def testwhiten():
-    strain_H1_whiten = util.whiten(strain_H1,psd_H1,dt)
-    strain_L1_whiten = util.whiten(strain_L1,psd_L1,dt)
-    try:
-        assert np.isclose(strain_H1_whiten[0], 648.1674991391047)
-    except AssertionError as detail:
-        print('The whitened H1 strain data is incorrect')
-        quit()
-    try:
-        assert np.isclose(strain_L1_whiten[0], -258.0340503594763)
-    except AssertionError as detail:
-        print('The whitened L1 strain data is incorrect')
-        quit() 
+def test_whiten():
+    fn_H1 = "data/H-H1_LOSC_4_V2-1126259446-32.hdf5"
+    fs = 4096
+    dt = time_H1[1] - time_H1[0]
+    NFFT = 4*fs
+    Pxx_H1, freqs = mlab.psd(strain_H1, Fs = fs, NFFT = NFFT)
+    psd_H1 = interp1d(freqs, Pxx_H1)
+    strain_H1_whiten = utils.whiten(strain_H1,psd_H1,dt)
+    assert strain_H1_whiten.shape == (131072, )
+    assert strain_H1_whiten.shape == strain_H1
+        
+def test_writewavefile(): 
+    utils.write_wavfile("audio/temp.wav", fs, data)
+    assert exists("audio/temp.wav")
+    remove("audio/temp.wav")
+    
+def test_reqshift():
+    fs = 4096
+    fshift = 400.
+    speedup = 1.
+    fss = int(float(fs)*float(speedup))
+    NFFT = 4*fs
+    Pxx_L1, freqs = mlab.psd(strain_L1, Fs = fs, NFFT = NFFT)
+    psd_L1 = interp1d(freqs, Pxx_L1)
+    strain_L1_whiten = utils.whiten(strain_L1,psd_L1,dt)
+    strain_L1_shifted = util.reqshift(strain_L1_whitenbp,fshift=fshift,sample_rate=fs)
+    assert straing_L1_shifted.shape == (131072,)
+
+def test_plot_functions():
+    pass
+    
+    
 
 
 
