8a52da143d3bac50f660f063df97a4aa30012ee7
Brandon Hong <hongbrandon333@berkeley.edu>
2022-04-26 01:07:51 +0000

    fixing errors in tests

--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.mlab as mlab
 from ligotools import readligo as rl
 from scipy.interpolate import interp1d
+from os.path import exists
+from os import remove
 
 strain_H1, time_H1, chan_dict_H1 = rl.loaddata('data/H-H1_LOSC_4_V2-1126259446-32.hdf5', 'H1')
 strain_L1, time_L1, chan_dict_L1 = rl.loaddata('data/L-L1_LOSC_4_V2-1126259446-32.hdf5', 'L1')
@@ -17,7 +19,6 @@ def test_whiten():
     psd_H1 = interp1d(freqs, Pxx_H1)
     strain_H1_whiten = utils.whiten(strain_H1,psd_H1,dt)
     assert strain_H1_whiten.shape == (131072, )
-    assert a.all(strain_H1_whiten.shape == strain_H1)
         
 def test_writewavefile(): 
     data = np.linspace(0,10,16000)
@@ -33,7 +34,7 @@ def test_reqshift():
     Pxx_L1, freqs = mlab.psd(strain_L1, Fs = fs, NFFT = NFFT)
     psd_L1 = interp1d(freqs, Pxx_L1)
     strain_L1_whiten = utils.whiten(strain_L1,psd_L1,dt)
-    strain_L1_shifted = util.reqshift(strain_L1_whitenbp,fshift=fshift,sample_rate=fs)
+    strain_L1_shifted = utils.reqshift(strain_L1_whitenbp,fshift=fshift,sample_rate=fs)
     assert straing_L1_shifted.shape == (131072,)
 
 def test_plot_functions():
